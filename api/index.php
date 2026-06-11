c9ff3a4ee4618e2342d43be4cfe7f1ff0b0fba8a
Shark Bounty <mateushexa@gmail.com>
2026-06-10 22:40:31 -0300

    heatm

--- api/index.php
+++ api/index.php
@@ -35,19 +35,14 @@ $ctaUrl = "https://moringa.vivalis.fit/?tk=smaow929as9";
         content="A lição mecânica do Ford RS200 que mudou a forma como os urologistas europeus encaram a saúde do homem após os 35." />
     <meta property="og:image" content="/images/stig-blomqvist-audi-quattro.jpg" />
 
-    <?php
-    $clarityId = getenv('CLARITY_PROJECT_ID') ?: 'x54hy9fdh4';
-    if (!empty($clarityId)):
-    ?>
     <!-- Microsoft Clarity Tracking Code -->
     <script type="text/javascript">
         (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "<?php echo htmlspecialchars($clarityId); ?>");
+        })(window, document, "clarity", "script", "x54hy9fdh4");
     </script>
-    <?php endif; ?>
 </head>
 
 <body>
